e34f1aa11c3bf1879d03f10a7f95566a000e27de
Kyle VanHaitsma <kylevh7@gmail.com>
2018-06-05 12:06:04 -0400

    initial commit

---
+++ designs.js
@@ -0,0 +1,15 @@
+// Select color input
+let color="";
+function changeColor(){
+     color=document.getElementById('colorPicker').value;
+    }
+
+// Select size input
+
+
+// When size is submitted by the user, call makeGrid()
+const canvas = document.getElementById('pixelCanvas');
+function makeGrid() {
+
+  // Your code goes here!
+}
